f015e4a07bedc6c5eeeedcf6320fe7e785dab770
giosurmanidze <giorgisurmanidze917@gmail.com>
2024-04-23 11:40:18 +0400

    feat/install packages for jwt auth

--- obj/Debug/net8.0/pokemon-review-api.AssemblyInfo.cs
+++ obj/Debug/net8.0/pokemon-review-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pokemon-review-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9b97cad95e45c94483554f24930eb992a78a0d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8744d3ec4dbe55ff08c4244d8fd516216cf2cf2")]
 [assembly: System.Reflection.AssemblyProductAttribute("pokemon-review-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pokemon-review-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
